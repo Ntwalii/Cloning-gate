4c1854a070b5b0efc9a43e0e27b8f231a06e444f
ntwalii <ntwaliaubin@gmail.com>
2024-05-17 11:11:24 +0200

    Study section ended and News init

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,10 @@ module.exports = {
         umutuku:"#FFF232",
         bannerGray:"rgb(51,51,51)",
         navGray:"#D5D5D5",
-        midBrown:"#A18A4D"
+        midBrown:"#A18A4D",
+        heroGray:"#363735",
+        heroBtnGray:"rgb(51,62,63)",
+        newsGray:"#F2F2F2"
       },
       fontFamily: {
         'roboto': ['Roboto', 'sans-serif'],
