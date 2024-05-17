9fc611189e6ae2db422958b247969a8b5f6c0f22
ntwalii <ntwaliaubin@gmail.com>
2024-05-17 09:57:31 +0200

    Navbar end

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,9 @@ module.exports = {
     extend: {
       colors:{
         umutuku:"#FFF232",
-        bannerGray:"rgb(51,51,51)"
+        bannerGray:"rgb(51,51,51)",
+        navGray:"#D5D5D5",
+        midBrown:"#A18A4D"
       },
       fontFamily: {
         'roboto': ['Roboto', 'sans-serif'],
