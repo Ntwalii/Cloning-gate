1577a73f929322b6cc4e8bd399dd054ca3f96a72
ntwalii <ntwaliaubin@gmail.com>
2024-05-17 08:55:24 +0200

    Cloning gate

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        umutuku:"#FFF232"
+      },
+      fontFamily:{
+        myFOnt:""
+      }
+    },
+  },
+  plugins: [],
+}
+
